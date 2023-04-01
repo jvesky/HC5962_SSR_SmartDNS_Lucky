a7321f75b2fdd5bafe0b19dec67a34f931438c72
jvesky <68280085+jvesky@users.noreply.github.com>
2023-04-01 21:50:32 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,6 +16,4 @@ rm -rf feeds/luci/themes/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
 sed -i 's/Bootstrap/Argon/g' feeds/luci/collections/luci/Makefile
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-rm -rf feeds/luci/applications/luci-app-mosdns
-cp -rf feeds/small8/luci-app-mosdns feeds/luci/applications/
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
